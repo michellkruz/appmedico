92714698bfa4fdc30173c085a7988175fbc3efc0
maryb <maryb@Voador>
2018-10-15 22:39:35 -0300

    Nova tentativa de csv->json

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -2,27 +2,27 @@
 //const csv = require('csvtojson');
 //let knn;
 
-	var csvFilePath = '5 doencas.csv'; // Data
+//	var csvFilePath = '5 doencas.csv'; // Data
+//
+//	csv().fromFile(csvFilePath).then((jsonObj)=>{
+//		console.log(jsonObj);
+//		/**
+//		 * [
+//		 * 	{a:"1", b:"2", c:"3"},
+//		 * 	{a:"4", b:"5". c:"6"}
+//		 * ]
+//		 */ 
+//	});
 
-	csv().fromFile(csvFilePath).then((jsonObj)=>{
-		console.log(jsonObj);
-		/**
-		 * [
-		 * 	{a:"1", b:"2", c:"3"},
-		 * 	{a:"4", b:"5". c:"6"}
-		 * ]
-		 */ 
-	});
 
 
-
-//csv({
-//	output: "csv"
-//})
-//.fromString("a,b,c\n1,2,3")
-//.then(function(result){
-//	console.log(result);
-//})
+csv({
+	output: "json"
+})
+.fromString("a,b,c\n1,2,3")
+.then(function(result){
+	console.log(result);
+})
 
 //csv()
 //.fromFile(csvFilePath)
