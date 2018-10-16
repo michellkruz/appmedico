a8859f4a17c3f20145f37eee6d3a178e96ddaf53
Michell Cruz <Michell Cruz@Stormrage>
2018-10-15 22:32:07 -0300

    Knn funcionando

---
+++ src/main/webapp/js/euclidean.js
@@ -0,0 +1,11 @@
+export function squaredEuclidean(p, q) {
+  let d = 0;
+  for (let i = 0; i < p.length; i++) {
+    d += (p[i] - q[i]) * (p[i] - q[i]);
+  }
+  return d;
+}
+
+export default function euclidean(p, q) {
+  return Math.sqrt(squaredEuclidean(p, q));
+}
