5f8f85a80c5fd3f421c6e431ae370a7274ee5e1f
Michell Cruz <Michell Cruz@Stormrage>
2018-11-01 01:22:15 -0300

    imagens novas doenças

--- src/main/webapp/plugins/fancybox/jquery.fancybox.js
+++ src/main/webapp/plugins/fancybox/jquery.fancybox.js
@@ -1913,7 +1913,7 @@
 						title.width( title.width() );
 					}
 
-					title.wrapInner('<span class="child"></span>');
+					title.wrapInner('<span id="titlegalery" class="child"></span>');
 
 					//Increase bottom margin so this title will also fit into viewport
 					F.current.margin[2] += Math.abs( getScalar(title.css('margin-bottom')) );
